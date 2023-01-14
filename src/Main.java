ae41d5eb576ab82661f3fbdd390fb9bbf90d16fd
Ivan Vasilev <tor419@yandex.ru>
2023-01-14 21:38:09 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        long cost = 10_000;
+
+        long miles = cost / 20;
+        System.out.println("Бонусы: " + miles);
+    }
+}
